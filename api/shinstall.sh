f98dfbab37564ca524a35bd66486be56d0f99924
Azeez Adewale <azeezadewale98@gmail.com>
2018-07-17 17:38:31 +0100

    online installer for windows, linux, unix oses

--- api/shinstall.sh
+++ api/shinstall.sh
@@ -9,7 +9,7 @@
 # curl -sSfL https://simple-lang.io/api/stable_version.sim to get version by url in future
 
 simple_lang_url="https://simple-lang.io?page=Download"
-setup_url_prefix="http://127.0.0.1/simple-lang/"
+setup_url_prefix="http://10.0.2.2/simple-lang/"
 temp_dir="${TMPDIR:-/tmp}"
 simple_lang_version="0.3.36"
 need_tty=yes
